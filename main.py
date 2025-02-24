64821265b5a5704b3e22941c68c1e53de70c2db0
Esteban <esteban@gmail.com>
2025-02-24 11:58:09 -0500

    Archivo Menu V1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from Estudiante import Estudiante
+
+lista_estudiante=[]
+
+def registrar_estudiante():
+    print("Se va a registrar un estudiante")
+    name=input("Ingrese el nombre del estudiante: ")
+    age=input("Ingrese la edad del estudiante: ")
+    address=input("Ingrese la dirección del estudiante: ")
+    course=input("El curso es: ")
+
+    estudiante=Estudiante(name, age, address, course)
+    lista_estudiante.append(Estudiante)
+
+    print("Estudiante registrado con éxito")
